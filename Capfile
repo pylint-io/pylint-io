117b1d741f360327a4f9de36079b9d3aa5a09d65
Jared <jared@muzolabs.com>
2017-08-26 12:11:29 -0600

    Add beta capistrano stage and prepare for secrets

--- Capfile
+++ Capfile
@@ -4,14 +4,6 @@ require "capistrano/setup"
 # Include default deployment tasks
 require "capistrano/deploy"
 
-# Load the SCM plugin appropriate to your project:
-#
-# require "capistrano/scm/hg"
-# install_plugin Capistrano::SCM::Hg
-# or
-# require "capistrano/scm/svn"
-# install_plugin Capistrano::SCM::Svn
-# or
 require "capistrano/scm/git"
 install_plugin Capistrano::SCM::Git
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,13 @@
 # config valid only for current version of Capistrano
 lock "3.9.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "pylint.io"
+set :repo_url, "git@github.com:pylint-io/pylint-io.git"
 
-# Default branch is :master
+set :branch, "master"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/data/www/pylint.io"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
