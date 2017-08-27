38aad38612edab3a81f315b7efbd4f2708d78a16
Jared <jared@muzolabs.com>
2017-08-26 22:51:23 -0600

    Clarify comments

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :rbenv_type, :user
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
-# Default value for linked_dirs is []
+# for puma
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
 
 # Default value for default_env is {}
